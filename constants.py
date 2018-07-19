fb5a3a66f5dac1ff3da332d418f625fdb0873e43
Lutfi221 <lutfi221@gmail.com>
2018-07-19 12:26:01 +0900

    Created constants.py
    To go by the rule DRY

---
+++ constants.py
@@ -0,0 +1,15 @@
+""" Where constants are defined
+"""
+
+URL = 'https://mncvision.id/schedule/table'
+SCHEDULE_TIMEZONE = '+0700'
+LOCAL_TIMEZONE = '+0900'
+
+CHANNEL_EXCLUSIONS = ['hd']
+
+WANTED_PROGRAMS = [
+    'kevin can wait',
+    'the simpsons',
+    'live pd',
+    'family guy'
+]
